a30e01e3f9938520685ca4448252325b33e299bf
Aidan Militzer <115504926+Choff-Clouds@users.noreply.github.com>
2023-02-17 18:31:15 -0500

    Update hospitalCharges.cpp

--- hospitalCharges.cpp
+++ hospitalCharges.cpp
@@ -6,26 +6,26 @@
 using namespace std;
 
 int main(){
-  Patient p1;
+  Patient patient;
   char temp;
   double temper;
   cout << "Enter I for in-patient or O for out-patient: ";
   cin >> temp;
-  p1.setPatientType(temp);
-  if(p1.getPatientType()=='I'){
+  patient.setPatientType(temp);
+  if(patient.getPatientType()=='I'){
     cout << endl << "Days spent in hospital: ";
     cin >> temper;
-    p1.setDays(temper);
+    patient.setDays(temper);
     cout << endl << "Daily room rate ($): ";
     cin >> temper;
-    p1.setRate(temper);
+    patient.setRate(temper);
   }
   cout << endl << "Service charges ($): ";
   cin >> temper;
-  p1.setServices(temper);
+  patient.setServices(temper);
   cout << endl << "Medication charges ($): ";
   cin >> temper;
-  p1.setMedication(temper);
-  cout << "Your total bill is ($): " << p1.calcTotalCharges();
+  patient.setMedication(temper);
+  cout << "Your total bill is ($): " << patient.calcTotalCharges();
 
 }
